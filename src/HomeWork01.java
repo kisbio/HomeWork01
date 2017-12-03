fc9cd648c281167a2265b2864d4e8ce74456dd92
KisBio <kisbio@gmail.com>
2017-12-03 19:25:53 +0100

    #1

---
+++ src/HomeWork01.java
@@ -0,0 +1,13 @@
+public class HomeWork01 {
+
+
+    public static void main(String[] args) {
+
+            int x = 5;
+            int y = 4;
+
+            System.out.println("A kerület = " + 2 * (x + y));
+            System.out.println("A terület = " + x * y);
+
+    }
+}

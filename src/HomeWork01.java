be9423a39992fd1740596ce6bc3fc2481de55fc3
KisBio <kisbio@gmail.com>
2017-12-03 20:46:12 +0100

    #3

--- src/HomeWork01.java
+++ src/HomeWork01.java
@@ -15,12 +15,22 @@ public class HomeWork01 {
     int dl = 52;
     int ml = dl*100;
     int cl = dl * 10;
-    float l = dl / 10;
+    double l = dl / 10;
     double hl = l / 100;
     //MEgadom, miket írjon ki.
     System.out.println(dl + " deciliter = " + ml + " mililiter");
     System.out.println(dl + " deciliter = " + cl + " centiliter");
     System.out.println(dl + " deciliter = " + l + " liter");
-    System.out.printf(dl + " deciliter = " + hl + " hektoliter");
+    System.out.println(dl + " deciliter = " + hl + " hektoliter");
+
+
+    //Harmadik feladat
+        //A sugár és a pi
+        int r = 42;
+        double pi = 3.14;
+        //Az eredmány
+        double volume = 4*r*r*r*pi/3;
+
+        System.out.println("A gömb térfogata: " + volume);
     }
 }

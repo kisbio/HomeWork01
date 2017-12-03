0d17e4def3ff150011f276be7d320837152f63fc
KisBio <kisbio@gmail.com>
2017-12-03 20:00:58 +0100

    #1

--- src/HomeWork01.java
+++ src/HomeWork01.java
@@ -2,10 +2,10 @@ public class HomeWork01 {
 
 
     public static void main(String[] args) {
-
+    //A téglalap oldalai:
             int x = 5;
             int y = 4;
-
+    //A kerülete és a területe
             System.out.println("A kerület = " + 2 * (x + y));
             System.out.println("A terület = " + x * y);
 

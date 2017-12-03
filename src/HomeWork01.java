4a64bf76b70b956dc87aa4a314b039a204e4655c
KisBio <kisbio@gmail.com>
2017-12-03 20:36:24 +0100

    #2

--- src/HomeWork01.java
+++ src/HomeWork01.java
@@ -9,5 +9,18 @@ public class HomeWork01 {
             System.out.println("A kerület = " + 2 * (x + y));
             System.out.println("A terület = " + x * y);
 
+
+            //Második feladat
+        //Megadom a váéltozókat:
+    int dl = 52;
+    int ml = dl*100;
+    int cl = dl * 10;
+    float l = dl / 10;
+    double hl = l / 100;
+    //MEgadom, miket írjon ki.
+    System.out.println(dl + " deciliter = " + ml + " mililiter");
+    System.out.println(dl + " deciliter = " + cl + " centiliter");
+    System.out.println(dl + " deciliter = " + l + " liter");
+    System.out.printf(dl + " deciliter = " + hl + " hektoliter");
     }
 }
